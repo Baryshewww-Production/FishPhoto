4fdedf8e17780785591a7a683ea92d1c92d05047
Baryshewww <baryshev-tm@mail.ru>
2023-03-01 15:10:30 +0300

    adds a module with helper functions

---
+++ js/util.js
@@ -0,0 +1,12 @@
+//Функция, которая возвращает целое положительное число из заданного диапазона
+const getRandomPositiveInteger = (first, second) => {
+  const lower = Math.ceil(Math.min(Math.abs(first), Math.abs(second)));
+  const upper = Math.floor(Math.max(Math.abs(first), Math.abs(second)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+//Функция, которая возвращает случайный элемент из переданного массива
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+export {getRandomArrayElement, getRandomPositiveInteger};
