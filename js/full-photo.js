819c8d035db32f90ab700164b7d949af1f435d96
Baryshewww <baryshev-tm@mail.ru>
2023-03-04 15:33:39 +0300

    adds a module with the functionality of opening a large image

---
+++ js/full-photo.js
@@ -0,0 +1,49 @@
+const socialCommentsList = document.querySelector('.social__comments');
+const bigPictureSection = document.querySelector('.big-picture');
+
+//Функция, которая делает видимым элемент(например, удаляет с него класс hidden)
+const getActiveFullPicture = (element, nameClass) => {
+  element.classList.remove(nameClass);
+};
+
+//Функция, которая делает невидимым элемент(например, добавляет ему класс hidden)
+const hideElement = (element, nameClass) => {
+  element.classList.add(nameClass);
+};
+
+const createCommentsUser = (arrayCommentsUser) => {
+  arrayCommentsUser.forEach((itemObj) => {
+    const commentTemplate = socialCommentsList.children[0].cloneNode(true);
+
+    const commentTemplateImg = commentTemplate.querySelector('.social__picture');
+    const commentTemplateText = commentTemplate.querySelector('.social__text');
+
+    commentTemplateImg.src = itemObj.avatar;
+    commentTemplateImg.alt = itemObj.name;
+    commentTemplateText.textContent = itemObj.message;
+
+    socialCommentsList.append(commentTemplate);
+  });
+  socialCommentsList.children[0].remove();
+  socialCommentsList.children[0].remove();
+};
+
+//Функция, которая открывает окно с большим изображением и показывает все данные и комментарии
+const showBigPicture = (userObject) => {
+  const bigPictureImg = bigPictureSection.querySelector('.big-picture__img').children[0];
+  const bigPictureLikesCount = bigPictureSection.querySelector('.likes-count');
+  const bigPictureCommentsCount = bigPictureSection.querySelector('.comments-count');
+  const bigPictureCaption = bigPictureSection.querySelector('.social__caption');
+  const bigPictureCount = bigPictureSection.querySelector('.social__comment-count');
+  const bigPictureLoader = bigPictureSection.querySelector('.comments-loader');
+
+  hideElement(bigPictureLoader, 'hidden');
+  hideElement(bigPictureCount, 'hidden');
+  createCommentsUser(userObject.comments);
+  bigPictureImg.src = userObject.url;
+  bigPictureLikesCount.textContent = userObject.likes;
+  bigPictureCommentsCount.textContent = userObject.comments.length;
+  bigPictureCaption.textContent = userObject.description;
+};
+
+export {showBigPicture, getActiveFullPicture, hideElement, bigPictureSection};
