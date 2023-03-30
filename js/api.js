9052e6102af0a8d766632786c9c8712592409fa9
Baryshewww <baryshev-tm@mail.ru>
2023-03-30 21:34:56 +0300

    Написал модуль с запросом к серверу и отправки данных на сервер

---
+++ js/api.js
@@ -0,0 +1,34 @@
+import {openSuccessSendMessage, openErrorSendMessage} from './errors.js';
+import {showAlert} from './util.js';
+
+const getData = (onSuccess) => {
+  fetch('https://27.javascript.pages.academy/kekstagram/data')
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then((posts) => {
+      onSuccess(posts);
+    })
+    .catch(() => {
+      showAlert('Не удалось получить данные с сервера :(');
+    });
+};
+
+const sendData = (onSuccess, onFail, body) => {
+  fetch('https://27.javascript.pages.academy/kekstagram',
+    {
+      method: 'POST',
+      body,
+    },
+  )
+    .then((response) => {
+      response.ok ? openSuccessSendMessage() : openErrorSendMessage();
+    })
+    .catch(() => {
+      openErrorSendMessage();
+    });
+};
+
+export {getData, sendData};
