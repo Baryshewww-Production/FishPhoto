ded26c0f0c9a35f22974de0848752b17a8100341
Baryshewww <baryshev-tm@mail.ru>
2023-04-02 22:23:54 +0300

    Добавил в модуль util.js новую функцию показа сообщения об ошибке

--- js/util.js
+++ js/util.js
@@ -1,3 +1,5 @@
+const ALERT_SHOW_TIME = 5000;
+
 // Функция, которая возвращает целое положительное число из заданного диапазона
 const getRandomPositiveInteger = (first, second) => {
   const lower = Math.ceil(Math.min(Math.abs(first), Math.abs(second)));
@@ -9,6 +11,14 @@ const getRandomPositiveInteger = (first, second) => {
 // Функция, которая возвращает случайный элемент из переданного массива
 const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
+function createIdGenerator() {
+  let lastGenerateId = 0;
+  return function() {
+    lastGenerateId += 1;
+    return lastGenerateId;
+  };
+}
+
 // Функция проверки нажатой клавиши, является ли она Escape или нет
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
@@ -17,9 +27,33 @@ const toggleElementHidden = (element, isHidden) => {
   element.classList.toggle('hidden', isHidden);
 };
 
+// Функция, которая показывает модальное уведомление об ошибки сервера
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+
+  alertContainer.style.zIndex = 100;
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.top = '0';
+  alertContainer.style.left = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '12px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+  document.body.appendChild(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+
 export {
   getRandomArrayElement,
   getRandomPositiveInteger,
+  createIdGenerator,
   isEscapeKey,
-  toggleElementHidden
+  toggleElementHidden,
+  showAlert
 };
